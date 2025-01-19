b53bc7cb7be326f69bf576f3135c62b1855db86c
vdpop <varadpuranik123@gmail.com>
2025-01-19 13:22:29 +0530

    removed all links

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,6 +1,5 @@
 import Navbar from './Navbar'
 import Image from 'next/image'
-import Link from "next/link";
 
 
 const Hero = () => {
--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
-  },
+  },
 };
 
 export default nextConfig;
