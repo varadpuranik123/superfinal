0ed306c7f386b53e4a6fbf313a238052a3600859
varad Puranik <92288624+varadpuranik123@users.noreply.github.com>
2025-01-19 13:07:49 +0530

    Update Navbar.tsx

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -7,11 +7,11 @@ const Navbar = () => {
             <div className="bg-clip-text h-[3.3rem]  tracking-tight font-denton lg:text-5xl text-2xl text-transparent bg-[linear-gradient(-145deg,_#fcfcfc_0%,_#999999_43%,_#fcfcfc_100%)] colorshadow ">
                 B for Blogs
             </div>
-            <Link href="/UploadBlogs" className="capitalize border border-[#333] bg-[#212121] hover:bg-[#313131] button transition ease-in-out duration-300 tracking-tight lg:text-xl text-sm font-semibold lg:px-6 lg:py-3 px-3 py-1">
-                <Link href="/UploadBlogs">
+            <a href="https://github.com/Mobiance/superlevel-hackathon" className="capitalize border border-[#333] bg-[#212121] hover:bg-[#313131] button transition ease-in-out duration-300 tracking-tight lg:text-xl text-sm font-semibold lg:px-6 lg:py-3 px-3 py-1">
+                <a href="https://github.com/Mobiance/superlevel-hackathon">
                     translate Blogs
-                </Link>
-            </Link>
+                </a>
+            </a>
         </div>
     )
 }
